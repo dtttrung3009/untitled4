48b6c4d6965c7869db86aae051006a09dff8f0bd
pumaover@gmail.com <pumaover@gmail.com>
2018-07-11 20:59:56 +0700

    gggg

---
+++ Main.java
@@ -0,0 +1,27 @@
+package com.trung;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        package Loop;
+import java.util.Scanner;
+        public class TimUocSoChung {
+            public static void main(String[] args) {
+                int a,b;
+                Scanner nhap = new Scanner(System.in);
+                System.out.println("Nhap a");
+                a= nhap.nextInt();
+                System.out.println("Nhap b");
+                b= nhap.nextInt();
+                a=Math.abs(a);
+                b=Math.abs(b);
+                if
+            }
+
+        }
+
+    }
+}
